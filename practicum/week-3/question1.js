f6cd71baa7f380448c2447a07d36ca48cfeb7bc2
Jiazheng <jasonhjz9527@gmail.com>
2021-02-19 15:19:51 -0800

    Updata

--- practicum/week-3/question1.js
+++ practicum/week-3/question1.js
@@ -1,14 +1,21 @@
 /*
-Given an array of numbers, input, 
-find whether the largest element in the array 
+Given an array of numbers, input,
+find whether the largest element in the array
 is at least twice as much as every other number
- in the array. If this is the case return true otherwise return false.
+in the array. If this is the case return true otherwise return false.
 
- Example:
-         input: [3,2,21,50]
-         output: true
+Example:
+        input: [3,2,21,50]
+        output: true
 */
 
-var largestNumberIsAtLeastTwice = function(input) {
-   
+var largestNumberIsAtLeastTwice = function (input) {
+  maxNum = Math.max(input) / 2;
+  var flag = 1;
+  for (var i = 0; i < input.length; i++) {
+    if (maxNum) {
+      flag;
+    }
+    return false;
+  }
 };
--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -1,4 +1,3 @@
-
 /*
 Given an integer array, input, 
 find the subarray
@@ -9,7 +8,4 @@ Example: input: [-12,3,-1,5,-2,1,-7]
                  because [3,-1,5] has the largest sum.
           */
 
- var largestSubarray = function(input) {
-     
-    
- };
+var largestSubarray = function (input) {};
